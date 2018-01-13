640ad2211a3002e5423b471bc5b6da5a6fab457e
Miranda Howitt <miranda.howitt@gmail.com>
2018-01-13 13:01:12 -0800

    Adding Personal Website files

---
+++ resources/js/script.js
@@ -0,0 +1,90 @@
+$(document).ready(function () {
+	$('.js--section-about').waypoint(function(direction) {
+		if (direction == "down") {
+			$('nav').addClass('follow');
+		} else {
+			$('nav').removeClass('follow');
+		}
+
+	}, {
+			offset: '40px;'
+	});
+
+	    /* Navigation scroll */
+    $(function() {
+      $('a[href*=\\#]:not([href=\\#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+
+ 	    /* Contact form and */
+
+$(function() {
+
+	// Get the form.
+	var form = $('#ajax-contact');
+
+	// Get the messages div.
+	var formMessages = $('#form-messages');
+
+	// Set up an event listener for the contact form.
+	$(form).submit(function(e) {
+		// Stop the browser from submitting the form.
+		e.preventDefault();
+
+		// Serialize the form data.
+		var formData = $(form).serialize();
+
+		// Submit the form using AJAX.
+		$.ajax({
+			url: "http://formspree.io/miranda.howitt@gmail.com",
+			method: "POST",
+			data: formData,
+			dataType: "json"
+		})
+		.done(function(response) {
+			// Make sure that the formMessages div has the 'success' class.
+			$(formMessages).removeClass('error');
+			$(formMessages).addClass('success');
+
+			// Set the message text.
+			$(formMessages).text(response);
+
+			// Clear the form.
+			$('#name').val('');
+			$('#email').val('');
+			$('#message').val('');
+		})
+		.fail(function(data) {
+			// Make sure that the formMessages div has the 'error' class.
+			$(formMessages).removeClass('success');
+			$(formMessages).addClass('error');
+
+			// Set the message text.
+			if (data.responseText !== '') {
+				$(formMessages).text(data.responseText);
+			} else {
+				$(formMessages).text('Oops! An error occured and your message could not be sent.');
+			}
+		});
+
+	});
+
+});
+
+
+
+
+
+
+
+});
