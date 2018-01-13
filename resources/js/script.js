cc1ab1d6c43f638cc4e1e98686ca4d51831ee45a
Miranda Howitt <miranda.howitt@gmail.com>
2018-01-13 15:13:32 -0800

    fix ajax and formspree

--- resources/js/script.js
+++ resources/js/script.js
@@ -30,42 +30,39 @@ $(document).ready(function () {
 
 $(function() {
 
-	// Get the form.
 	var form = $('#ajax-contact');
 
-	// Get the messages div.
 	var formMessages = $('#form-messages');
 
-	// Set up an event listener for the contact form.
+	
 	$(form).submit(function(e) {
-		// Stop the browser from submitting the form.
+		
 		e.preventDefault();
 
-		// Serialize the form data.
 		var formData = $(form).serialize();
 
 		// Submit the form using AJAX.
 		$.ajax({
-			url: "https://formspree.io/miranda.howitt@gmail.com",
-			method: "POST",
+			url: '//formspree.io/miranda.howitt@gmail.com',
+			method: 'POST',
 			data: formData,
 			dataType: "json"
 		})
 		.done(function(response) {
-			// Make sure that the formMessages div has the 'success' class.
+			
 			$(formMessages).removeClass('error');
 			$(formMessages).addClass('success');
 
 			// Set the message text.
 			$(formMessages).text(response);
 
-			// Clear the form.
+			// Clearing the form.
 			$('#name').val('');
 			$('#email').val('');
 			$('#message').val('');
 		})
 		.fail(function(data) {
-			// Make sure that the formMessages div has the 'error' class.
+
 			$(formMessages).removeClass('success');
 			$(formMessages).addClass('error');
 
